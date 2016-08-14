3b9e1efab2c378d7c0d1ba57dbdaa19fb6b0a4a1
jmoscati <Joseph.moscatiello@gmail.com>
2016-08-13 23:34:47 -0400

    Continued adding tests for demos

--- app/src/main/java/demo/moss/com/aug30demo/MainActivity.java
+++ app/src/main/java/demo/moss/com/aug30demo/MainActivity.java
@@ -1,13 +1,41 @@
 package demo.moss.com.aug30demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        text = (TextView) findViewById(R.id.text);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeText();
+            }
+        });
     }
+
+    public void changeText(){
+
+        String textValue = text.getText().toString();
+
+        if(textValue.equalsIgnoreCase(getString(R.string.world)))
+            text.setText(getString(R.string.universe));
+        else
+            text.setText(getString(R.string.world));
+    }
+
 }
