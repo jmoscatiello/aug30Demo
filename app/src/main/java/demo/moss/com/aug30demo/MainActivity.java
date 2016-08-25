6cf62b7595191662be687b52bbd85b053a9a17ff
Joseph Moscatiello <Joseph.Moscatiello@capitalone.com>
2016-08-24 23:29:59 -0400

    Pro guard rule changes

--- app/src/main/java/demo/moss/com/aug30demo/MainActivity.java
+++ app/src/main/java/demo/moss/com/aug30demo/MainActivity.java
@@ -1,12 +1,12 @@
 package demo.moss.com.aug30demo;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     Button button;
 
---
+++ app/src/test/java/MainActivityTest.java
@@ -0,0 +1,58 @@
+package demo.moss.com.aug30demo;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.Assert.assertNotNull;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import static org.junit.Assert.assertNotNull;
+
+
+
+/**
+ * Created by josephmoscatiello1 on 16-08-13.
+ */
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class MainActivityTest {
+
+    MainActivity activity;
+
+    @Before
+    public void setup() {
+        // Convenience method to run MainActivity through the Activity Lifecycle methods:
+        // onCreate(...) => onStart() => onPostCreate(...) => onResume()
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void shouldHaveHappySmiles() throws Exception {
+        String hello = activity.getResources().getString(
+                R.string.world);
+        assertThat(hello, equalTo("Hello World"));
+    }
+
+    @Test
+    public void testTakeScreenshot() {
+
+        assertNotNull(activity);
+
+        //onView(withId(R.id.fab)).perform(click());
+
+        Screengrab.screenshot("afterFabClick");
+    }
+
+}
